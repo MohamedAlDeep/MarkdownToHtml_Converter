c242d6de85cc2589fd49062b9ed508a09697d199
aldeep <aldeep@hackclub.app>
2025-01-04 12:50:01 +0000

    Added syntax highlighting for code

--- app.js
+++ app.js
@@ -12,7 +12,54 @@ const upload = multer({ dest: 'uploads/' });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    const readmePath = path.join(__dirname, 'uploads', 'readme.md');
 
+    fs.readFile(readmePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error reading README.md file.');
+        }
+
+        const htmlContent = marked(data);
+        const styledHtmlContent = `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Markdown to HTML</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css">
+                
+                
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css">
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+
+                <!-- and it's easy to individually load additional languages -->
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/go.min.js"></script>
+
+                <script>hljs.highlightAll();</script>
+                
+                <style>
+                    body {
+                        box-sizing: border-box;
+                        min-width: 200px;
+                        max-width: 980px;
+                        margin: 0 auto;
+                        padding: 45px;
+                    }
+                </style>
+            </head>
+            <body>
+                <article class="markdown-body">
+                    ${htmlContent}
+                </article>
+            </body>
+            </html>
+        `;
+
+        res.send(styledHtmlContent);
+    });
+});
 
 app.post('/convert', upload.single('markdownFile'), (req, res) => {
     if (!req.file) {
@@ -28,34 +75,34 @@ app.post('/convert', upload.single('markdownFile'), (req, res) => {
 
         const htmlContent = marked(data);
         const styledHtmlContent = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Markdown to HTML</title>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css">
-            <style>
-                body {
-                    box-sizing: border-box;
-                    min-width: 200px;
-                    max-width: 980px;
-                    margin: 0 auto;
-                    padding: 45px;
-                }
-            </style>
-        </head>
-        <body>
-            <article class="markdown-body">
-                ${htmlContent}
-            </article>
-        </body>
-        </html>
-    `;
-        res.send(styledHtmlContent)
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Markdown to HTML</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css">
+                <style>
+                    body {
+                        box-sizing: border-box;
+                        min-width: 200px;
+                        max-width: 980px;
+                        margin: 0 auto;
+                        padding: 45px;
+                    }
+                </style>
+            </head>
+            <body>
+                <article class="markdown-body">
+                    ${htmlContent}
+                </article>
+            </body>
+            </html>
+        `;
+
+        res.send(styledHtmlContent);
     });
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
